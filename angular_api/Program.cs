67cbf9e361bbcc63c4ad810514e42a0b4b6152f2
Karthik707 <karthikkulal707@gmail.com>
2022-10-16 19:20:07 +0530

    Add project files.

---
+++ angular_api/Program.cs
@@ -0,0 +1,40 @@
+
+using angular_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configure the sql connectio
+//Configure Database Connection Strings
+builder.Services.AddDbContext<TE_API_AngularContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TEConnection")));
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Cross-Origin Resource Sharing - ACCESS THE API IN VARIOUS APPLICATIONS
+builder.Services.AddCors(c => {
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+var app = builder.Build();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
